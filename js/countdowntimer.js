4ae69bb0d75249b09e3ce737fb4da8cd7533f2b4
maggieanddan <markymarc7979@gmail.com>
2018-07-10 16:50:50 -0700

    first website upload

---
+++ js/countdowntimer.js
@@ -0,0 +1,33 @@
+var deadline = 'October 13 2018 23:59:59 GMT+0200';
+
+function getTimeRemaining(endtime){
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor( (t/1000) % 60 );
+  var minutes = Math.floor( (t/1000/60) % 60 );
+  var hours = Math.floor( (t/(1000*60*60)) % 24 );
+  var days = Math.floor( t/(1000*60*60*24) );
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+// getTimeRemaining(deadline).minutes
+
+function initializeClock(id, endtime){
+  var clock = document.getElementById(id);
+  var timeinterval = setInterval(function(){
+    var t = getTimeRemaining(endtime);
+    clock.innerHTML = 'days: ' + t.days + '<br>' +
+                      'hours: '+ t.hours + '<br>' +
+                      'minutes: ' + t.minutes + '<br>' +
+                      'seconds: ' + t.seconds;
+    if(t.total<=0){
+      clearInterval(timeinterval);
+    }
+  },1000);
+}
+
+initializeClock('clockdiv', deadline);
---
+++ js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+
+	/* ---- Countdown timer ---- */
+
+	$('#counter').countdown({
+		timestamp : (new Date(2018,8,19)).getTime() + 1000
+	});
+
+
+	/* ---- Animations ---- */
+
+	$('#links a').hover(
+		function(){ $(this).animate({ left: 3 }, 'fast'); },
+		function(){ $(this).animate({ left: 0 }, 'fast'); }
+	);
+
+	$('footer a').hover(
+		function(){ $(this).animate({ top: 3 }, 'fast'); },
+		function(){ $(this).animate({ top: 0 }, 'fast'); }
+	);
+
+
+
+
+});
